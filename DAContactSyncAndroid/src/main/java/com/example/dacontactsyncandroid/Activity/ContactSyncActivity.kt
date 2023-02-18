c4e9ec859e2341abf146394ce0d6fc5329ad4442
jaydipdioneapps <jaydip@dioneapps.com>
2023-02-18 14:55:12 +0530

    project initialized

---
+++ DAContactSyncAndroid/src/main/java/com/example/dacontactsyncandroid/Activity/ContactSyncActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dacontactsyncandroid.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dacontactsyncandroid.R
+
+class ContactSyncActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_sync)
+
+    }
+}
